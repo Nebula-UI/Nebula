a6d1994420cf86dd58d7aca2dab7cf6bb1fe8f03
ashwinh <ashwinh@cybage.com>
2014-01-24 01:01:17 +0530

    Updates comment in events.js file;
    Signed-off-by: ashwinh <ashwinh@cybage.com>

--- src/core/events.js
+++ src/core/events.js
@@ -1,11 +1,11 @@
-/*
-*   This module loads event object and it's dependencies. It gives
-*   the ability to publish, subscribe and unsubscribe to custom application events.
-*
-*   NOTE: This object uses Backbone events for publishing, subscribing and
-*   unsubscribing events. For more information refer to Backbone.Events documentation.
-*   http://documentcloud.github.com/backbone/#Events
-*/
+/***
+ * This module loads event object and it's dependencies. It gives
+ * the ability to publish, subscribe and unsubscribe to custom application events.
+ *
+ * NOTE: This object uses Backbone events for publishing, subscribing and
+ * unsubscribing events. For more information refer to Backbone.Events documentation.
+ * http://documentcloud.github.com/backbone/#Events
+ */
 
 define(function(require) {
 	'use strict';
