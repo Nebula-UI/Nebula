412e2350e487410ba368234dc0e8214f9bc6dd08
hegdeashwin <ashwin.hegde3@gmail.com>
2014-07-05 23:08:53 +0530

    Re-factors Gruntfile configuration.
    Signed-off-by: hegdeashwin <ashwin.hegde3@gmail.com>

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,10 +6,10 @@ module.exports = function(grunt) {
      */
     require('time-grunt')(grunt);
 
+    /**
+     * Setup configuration
+     */
     grunt.initConfig({
-        /**
-         * Setup configuration
-         */
         clean: {
             build: ["prod"]
         },
@@ -22,85 +22,13 @@ module.exports = function(grunt) {
             all: ["src/apps/**/*.js"],
             options: {
                 ignores: ["src/main.js", "src/systems/libs/**/*.js", "src/systems/frameworks/**/*.js"],
-                "strict": true,
-                "curly": true,
-                "eqeqeq": true,
-                "immed": true,
-                "latedef": true,
-                "newcap": true,
-                "noarg": true,
-                "sub": true,
-                "undef": false,
-                "boss": true,
-                "eqnull": true,
-                "browser": true,
-                "unused": true,
-                "proto": true,
-                "globals": {
-                    "jQuery": true,
-                    "define": true,
-                    "require": true,
-                    "console": true,
-                    "$": true,
-                    "_": true,
-                    "asyncTest": true,
-                    "deepEqual": true,
-                    "equal": true,
-                    "expect": true,
-                    "module": true,
-                    "notDeepEqual": true,
-                    "notEqual": true,
-                    "notStrictEqual": true,
-                    "ok": true,
-                    "QUnit": true,
-                    "raises": true,
-                    "start": true,
-                    "stop": true,
-                    "strictEqual": true,
-                    "test": true,
-                    "throws": true,
-                    "prompt": true,
-                    "alert": true
-                }
+                jshintrc: 'config/.jshintrc'
             }
         },
-        /**
-         * Note*: For `jscs` task : few configuration setting are turned off which can be change as per your requirements.
-         */
         jscs: {
             src: "src/apps/**/*.js",
             options: {
-                "excludeFiles": ["src/systems/libs/**/*.js", "src/main.js"],
-                "requireCurlyBraces": ["if", "else", "for", "while", "do", "try", "catch"],
-                "requireSpaceAfterKeywords": ["if", "else", "for", "while", "do", "switch", "return", "try", "catch"],
-                // "requireSpaceBeforeBinaryOperators": true,
-                // "requireSpaceBeforePostfixUnaryOperators": true,
-                // "requireSpacesInConditionalExpression": {
-                //     "afterTest": true,
-                //     "beforeConsequent": true,
-                //     "afterConsequent": true,
-                //     "beforeAlternate": true
-                // },
-                // "requireSpaceAfterBinaryOperators": true,
-                // "requireSpaceAfterPrefixUnaryOperators": true,
-                // "disallowSpaceBeforeBinaryOperators": true,
-                // "disallowSpaceBeforePostfixUnaryOperators": true,
-                // "disallowSpacesInConditionalExpression": {
-                //     "afterTest": true,
-                //     "beforeConsequent": true,
-                //     "afterConsequent": true,
-                //     "beforeAlternate": true
-                // },
-                // "disallowSpaceAfterBinaryOperators": true,
-                // "disallowSpaceAfterPrefixUnaryOperators": true,
-                "disallowImplicitTypeConversion": ["string"],
-                "disallowKeywords": ["with"],
-                "disallowMultipleLineBreaks": true,
-                "disallowKeywordsOnNewLine": ["else"],
-                "validateJSDoc": {
-                    "checkParamNames": true,
-                    "requireParamTypes": true
-                }
+                config: "config/.jscsrc"
             }
         },
         csslint: {
@@ -109,60 +37,14 @@ module.exports = function(grunt) {
                     "src/stylesheets/css/**/*.css"
                 ],
                 options: {
-                    "ids": false,
-                    "overqualified-elements": false,
-                    "important": false,
-                    "adjoining-classes": false,
-                    "known-properties": false,
-                    "box-sizing": false,
-                    "box-model": false,
-                    "display-property-grouping": false,
-                    "bulletproof-font-face": false,
-                    "compatible-vendor-prefixes": false,
-                    "regex-selectors": false,
-                    "errors": false,
-                    "duplicate-background-images": false,
-                    "duplicate-properties": false,
-                    "empty-rules": false,
-                    "selector-max-approaching": false,
-                    "gradients": false,
-                    "fallback-colors": false,
-                    "font-sizes": false,
-                    "font-faces": false,
-                    "floats": false,
-                    "star-property-hack": false,
-                    "outline-none": false,
-                    "import": false,
-                    "underscore-property-hack": false,
-                    "rules-count": false,
-                    "qualified-headings": false,
-                    "selector-max": false,
-                    "shorthand": false,
-                    "text-indent": false,
-                    "unique-headings": false,
-                    "universal-selector": false,
-                    "unqualified-attributes": false,
-                    "vendor-prefix": true,
-                    "zero-units": false
+                    csslintrc: 'config/.csslintrc'
                 }
             }
         },
         htmlhint: {
             Root_HTML_Files: {
                 options: {
-                    "tag-pair": true,
-                    "tagname-lowercase": true,
-                    "attr-lowercase": true,
-                    "attr-value-double-quotes": true,
-                    "attr-value-not-empty": true,
-                    "doctype-first": true,
-                    "tag-self-close": true,
-                    "spec-char-escape": true,
-                    "id-unique": true,
-                    "head-script-disabled": true,
-                    "img-alt-require": true,
-                    "doctype-html5": true,
-                    "id-class-value": true
+                    htmlhintrc: 'config/.htmlhint-n-rc'
                 },
                 src: [
                     "src/*.html"
@@ -170,17 +52,7 @@ module.exports = function(grunt) {
             },
             Templates: {
                 options: {
-                    "tagname-lowercase": true,
-                    "attr-lowercase": true,
-                    "attr-value-double-quotes": true,
-                    "attr-value-not-empty": true,
-                    "tag-self-close": false,
-                    "spec-char-escape": true,
-                    "id-unique": true,
-                    "head-script-disabled": true,
-                    "img-alt-require": true,
-                    "id-class-value": true,
-                    "style-disabled": true
+                    htmlhintrc: 'config/.htmlhint-t-rc'
                 },
                 src: [
                     "src/templates/**/*.html"
@@ -249,6 +121,31 @@ module.exports = function(grunt) {
                     nodes: ["console.log", "debug"]
                 }
             }
+        },
+        autoprefixer: {
+            options: {
+                "browsers": ["ie 8", "ie 9", "Firefox >= 17", "ios 7", "last 10 Chrome versions", "last 2 Safari versions", "Android 4"]
+            },
+
+            multiple: {
+                expand: true,
+                flatten: true,
+                src: 'src/stylesheets/css/common/*.css',
+                dest: 'src/stylesheets/css/common/'
+            }
+        },
+        htmlmin: {
+            dist: {
+                options: {
+                    removeComments: true,
+                    collapseWhitespace: true
+                },
+                files: {
+                    "prod/index.html": "prod/index.html",
+                    "prod/templates/homeTpl.html": "prod/templates/homeTpl.html",
+                    "prod/templates/aboutTpl.html": "prod/templates/aboutTpl.html"
+                }
+            }
         }
     });
 
@@ -256,28 +153,55 @@ module.exports = function(grunt) {
      * Load tasks
      */
     grunt.loadNpmTasks("grunt-contrib-clean");
+    grunt.loadNpmTasks("grunt-contrib-watch");
+    grunt.loadNpmTasks("grunt-strip");
     grunt.loadNpmTasks("grunt-shell");
     grunt.loadNpmTasks("grunt-contrib-jshint");
     grunt.loadNpmTasks("grunt-jscs-checker");
+    grunt.loadNpmTasks('grunt-contrib-less');
     grunt.loadNpmTasks("grunt-contrib-csslint");
     grunt.loadNpmTasks('grunt-htmlhint');
-    grunt.loadNpmTasks('grunt-contrib-less');
-    grunt.loadNpmTasks("grunt-contrib-watch");
+    grunt.loadNpmTasks('grunt-contrib-htmlmin');
+    grunt.loadNpmTasks('grunt-autoprefixer');
     grunt.loadNpmTasks("grunt-qunit-istanbul");
-    grunt.loadNpmTasks("grunt-strip");
 
     /**
-     * Define tasks : Tasks for Development eco - system.
+     * Define tasks : Tasks for development eco - system.
      */
-    grunt.registerTask("default", ["jshint", "jscs", "htmlhint", "less:readyMade", "less:customMade", "csslint"]);
+    grunt.registerTask("default", [
+        "htmlhint",
+        "csslint",
+        "jshint",
+        "jscs",
+        "less:readyMade",
+        "less:customMade",
+        "autofix"
+    ]);
+    grunt.registerTask('dev', ['default']); // Alias for `default`.
 
     /**
      * Define tasks : Tasks for build eco - system.
      */
-    grunt.registerTask("build", ["clean", "jshint", "jscs", "htmlhint", "less:prod", "csslint", "shell", "strip"]);
+    grunt.registerTask("build", [
+        "htmlhint",
+        "csslint",
+        "jshint",
+        "compileless",
+        "autofix",
+        "clean",
+        "shell",
+        "strip",
+        "htmlmin"
+    ]);
 
     /**
      * Define sub-tasks : Tasks for Less compilation.
      */
     grunt.registerTask("compileless", ["less:readyMade", "less:customMade"]);
+
+    /**
+     * Define sub-tasks : Alias for `autofix`
+     */
+    grunt.registerTask('autofix', ['autoprefixer']);
+
 };
