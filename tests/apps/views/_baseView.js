3acd490f362fe07ce28b72a9a35d03c471fc3b89
ashwinh <ashwinh@cybage.com>
2014-10-11 12:31:02 +0530

    Re-factor default test cases

--- tests/apps/views/_baseView.js
+++ tests/apps/views/_baseView.js
@@ -1,8 +1,37 @@
-(function(require) {
-    'use strict';
+define(function(require) {
+	"use strict";
 
-    test('Test Case 1', function() {
-        strictEqual(1, 1, 'Compare two values.');
-    });
+	module("Test Module Name A", {
+		setup: function() {
+			// Prepare something for all following tests.
+			console.log("Todo: Setup Executed.")
+		},
+		teardown: function() {
+			// Clean up after each test.
+			console.log("Todo: Teardown Executed.")
+		}
+	});
 
-})();
+	test("Equal assertion", function() {
+		equal(1, "1", "String '1' and number 1 have the same value");
+	});
+
+	test("Strict equal assertion", function() {
+		strictEqual(1, 1, "1 and 1 have the same value and type");
+	});
+
+	test("Deep equal assertion", function() {
+		var obj = {
+			foo: "bar"
+		};
+
+		deepEqual(obj, {
+			foo: "bar"
+		}, "Two objects can be the same in value");
+	});
+
+	test("ok test", function() {
+		ok(true, "true succeeds");
+	});
+
+});
